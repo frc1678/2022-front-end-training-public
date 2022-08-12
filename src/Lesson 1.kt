8e86c360ac3cd194946ff85a5e14a5333848f343
Alison Lin <totorodog576@gmail.com>
2022-08-11 17:48:02 -0700

    Initial commit

---
+++ src/Lesson 1.kt
@@ -0,0 +1,4 @@
+// Remember to check in with a veteran member if you need help and once you've finished!
+fun main() {
+    // Write lesson 1 code in this main function!
+}
---
+++ src/Lesson 4.kt
@@ -0,0 +1,4 @@
+// Remember to check in with a veteran member if you need help and once you've finished!
+fun main() {
+    // Write lesson 4 code in this main function!
+}
